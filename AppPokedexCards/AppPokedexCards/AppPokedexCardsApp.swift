9f7315300671aa86eda28989179b27d181714f69
Ricardo Massaki <ricardo29puruba@gmail.com>
2025-05-05 11:39:09 -0300

    initial commit

---
+++ AppPokedexCards/AppPokedexCards/AppPokedexCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPokedexCardsApp.swift
+//  AppPokedexCards
+//
+//  Created by Ricardo Massaki on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AppPokedexCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppPokedexCards/AppPokedexCardsTests/AppPokedexCardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppPokedexCardsTests.swift
+//  AppPokedexCardsTests
+//
+//  Created by Ricardo Massaki on 05/05/25.
+//
+
+import Testing
+@testable import AppPokedexCards
+
+struct AppPokedexCardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
